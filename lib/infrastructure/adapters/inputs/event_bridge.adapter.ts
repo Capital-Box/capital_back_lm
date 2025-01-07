c9e7b44f9d154e734a3de9e5a205af41f288ad14
Ignacio Ariel Cordoba <nachocordoba05@gmail.com>
2025-01-07 15:56:13 -0300

    feat(event_bridge): Adding EventBridgeAdapter

---
+++ lib/infrastructure/adapters/inputs/event_bridge.adapter.ts
@@ -0,0 +1,20 @@
+import { EventBridgeEvent } from "aws-lambda";
+import { EventBridgePort } from "../../ports/inputs/event_bridge.port";
+import { InputAdapter } from "./input.adapter";
+import { ResponseDTO } from "../../dtos/response.dto";
+import { Exception } from "../../../shared/exceptions/exception";
+import { RequestDTO } from "../../dtos/request.dto";
+import { UnexpectedException } from "../../../shared/exceptions/unexpected.exception";
+import { EventBridgeMapper } from "../../mappers/event_bridge.mapper";
+
+export abstract class EventBridgeAdapter<EventString extends string, EventBody>
+  extends InputAdapter<EventBridgeEvent<EventString, EventBody>, void>
+  implements EventBridgePort<EventString, EventBody>
+{
+  async handle(event: EventBridgeEvent<EventString, EventBody>): Promise<void> {
+    const request = EventBridgeMapper.toRequestDTO(event);
+    await this.invoke(request);
+  }
+
+  protected abstract invoke(req: RequestDTO): Promise<ResponseDTO>;
+}
